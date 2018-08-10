9f3b723b9830d04ebd972971172c3562dc4408e1
Nicolas Porcel <nicolasporcel06@gmail.com>
2018-08-10 22:18:56 +0200

    Improve shortenUrl service

--- src/main/java/com/UrlShortener/webapp/UrlShortenerWebservice.java
+++ src/main/java/com/UrlShortener/webapp/UrlShortenerWebservice.java
@@ -10,14 +10,20 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.UrlShortener.service.UrlShortenerService;
 import com.UrlShortener.service.UrlStoreService;
  
 @Path("/")
 public class UrlShortenerWebservice {
+
+    private final String servicePrefix = "http://example.com/";
 
     private static class UrlStoreServiceMock implements UrlStoreService {
         public long currentIndex = 0;
@@ -37,8 +43,10 @@ public class UrlShortenerWebservice {
     @POST
     @Path("/shorten_url")
     @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     public Response shortenUrl(InputStream incomingData) {
         String urlToShorten = "";
+        String slug = "";
 
         // Read input
         try {
@@ -48,20 +56,40 @@ public class UrlShortenerWebservice {
             while ((line = in.readLine()) != null) {
                 inputMessage.append(line);
             }
+
+            JSONObject parsedInput = new JSONObject(inputMessage.toString());
+            for (String key : parsedInput.keySet()) {
+                if (key != "url") {
+                    throw new JSONException("InvalidKey");
+                }
+            }
+
+            urlToShorten = parsedInput.getString("url");
         } catch (Exception e) {
             return Response.status(500).entity("Invalid POST data.").build();
         }
 
         // Shorten URL
         try {
-            service.shortenUrl(urlToShorten);
+            slug = service.shortenUrl(urlToShorten);
         } catch (MalformedURLException e) {
             return Response.status(500).entity("The URL is malformed.").build();
         } catch (IOException e) {
             return Response.status(500).entity("Unable to connect to the URL.").build();
+        } catch (Exception e) {
+            return Response.status(500).entity("Unable to shorten the URL.").build();
         }
 
-        return Response.status(201).entity("").build();
+        String shortenedUrl = this.servicePrefix + slug;
+
+        // Encode response
+        try {
+            String response = new JSONObject()
+                .put("shortened_url", shortenedUrl).toString();
+            return Response.status(201).entity(response).build();
+        } catch (Exception e) {
+            return Response.status(500).entity("Unable to encode response.").build();
+        }
     }
 
     @GET
