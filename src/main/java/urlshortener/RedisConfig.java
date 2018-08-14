e2cfdbbeacf40471375827311fcd09787fdf4d03
Nicolas Porcel <nicolasporcel06@gmail.com>
2018-08-14 02:24:09 +0200

    Add serializer

--- src/main/java/urlshortener/RedisConfig.java
+++ src/main/java/urlshortener/RedisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
 
 @Configuration
 @ComponentScan("urlshortener")
@@ -13,6 +14,7 @@ public class RedisConfig {
     RedisTemplate<Long, String> redisTemplate(RedisConnectionFactory factory) {
         final RedisTemplate<Long, String> template = new RedisTemplate<>();
         template.setConnectionFactory(factory);
+        template.setKeySerializer(new GenericToStringSerializer<Long>(Long.class));;
         return template;
     }
 }
